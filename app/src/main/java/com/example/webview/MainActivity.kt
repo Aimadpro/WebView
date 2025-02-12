be5fe2c14c123ec1f6595be11648a9c58479e19c
aimad <aaisad2324@politecnics.barcelona>
2025-02-12 16:50:19 +0100

    op

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.webview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.webview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-
+        val webSettings = binding.webView.settings
+        webSettings.javaScriptEnabled = true
+        setContentView(binding.root)
+        binding.webView.loadUrl("file:///android_asset/index.html")
     }
 
 }
